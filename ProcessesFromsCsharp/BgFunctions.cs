ed8e8e4d2dd6a7395231d78a10f24563e5c5fb44
n0obed <nikhilprem25@gmial.com>
2022-03-11 22:49:56 +0530

    first version, no time, battery display yet.

---
+++ ProcessesFromsCsharp/BgFunctions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+
+// TODO complete addProcess function to right click and exit any of the for list.
+namespace ProcessesFromsCsharp
+{
+    public class BgFunctions
+    {
+        public List<string> process_names_useless = new List<string>
+        {
+            "System", "Registry", "winitt", "services", "svchost", "NVDisplay.Container", "Memory Compression", "dwm",
+            "Microsoft.Photos", "YourPhone", "NisSrv", "HelpPane", "SystemSettings", "Idle", "wininit", "wlanext", "igfxCUIServiceN",
+            "spoolsv", "SearchIndexer", "taskhostw", "RstMwService", "RtkAudUService64", "conhost", "MSBuild", "WebViewHost",
+            "ServiceHub.VSDetouredHost", "ServiceHub.IdentityHost", "ServiceHub.SettingsHost", "ServiceHub.Host.CLR.x86",
+            "ServiceHub.ThreadedWaitDialog", "ServiceHub.RoslynCodeAnalysisService", "ServiceHub.Host.CLR.x64", "ServiceHub.Host.CLR.x86",
+            "ServiceHub.DataWarehouseHost", "msedgewebview2", "Microsoft.ServiceHub.Controller", "SecurityHealthService",
+            "SecurityHealthSystray", "ApplicationFrameHost", "SgrmBroker", "StartMenuExperienceHost", "SearchApp", "TextInputHost",
+            "jhi_service", "MsMpEng", "WUDFHost", "fontdrvhost", "winlogon", "LsaIso", "lsass", "rundll32", "PerfWatson2",
+            "Secure System", "sihost", "NVIDIA Web Helper", "GoogleCrashHandler", "GoogleCrashHandler64", "SettingSyncHost",
+            "explorer", "ShellExperienceHost", "LockApp", "SystemSettingsBroker", "TrustedInstaller", "cmd", "MpCopyAccelerator",
+            "fodhelper", "TiWorker", "devenv", "dasHost", "DataExchangeHost", "python", "MusNotifyIcon", "StandardCollector.Service",
+            "VsDebugConsole", "VBCSCompiler", "SearchProtocolHost", "SearchFilterHost", "smss", "UserOOBEBroker", "Time",
+            "deskpins", "RuntimeBroker", "csrss", "WmiPrvSE", "audiodg", "GameBarPresenceWriter", "smartscreen",
+            "vmcompute", "RtkUWP", "OfficeClickToRun", "dllhost", "MoUsoCoreWorker", "CompPkgSrv", "Video.UI", "PaintStudio.View", "Calculator",
+            "IntelCpHDCPSvc", "nvcontainer", "LMS", "ELANFPService", "ctfmon", "notepad", "YourPhoneServer", "SnippingTool",
+            "steamwebhelper", "steamservice", "chrome"
+        };
+        public List<string> process_names = new List<string>
+        {
+            "chrome", "csgo", "Cities", "Bannerlord", "steam", "Teams"
+        };
+        
+        public bool inProcesses(string word)
+        {
+            bool toggle = false;
+            foreach (string s in process_names)
+            {
+                if (s == word)
+                {
+                    toggle = true;
+                }
+            }
+            return toggle;
+        }
+
+    }
+}
+
---
+++ ProcessesFromsCsharp/Form1.cs
@@ -0,0 +1,225 @@
+using System;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace ProcessesFromsCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Process[] processList;
+        public Form1()
+        {
+            InitializeComponent();
+           // Process.Start("cmd.exe", "/k ECHO hello Nikhil!");
+           this.ShowInTaskbar = false;
+            this.Text = "Processes";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            renderProcessesOnListView();
+            processesInNotifyIcon(processList);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            refreshEvent();
+        }
+
+        // Note: This function is not mine, find it here
+        // https://ourcodeworld.com/articles/read/439/how-to-list-all-windows-processes-and-their-attributes-task-manager-like-with-c-in-winforms
+        public void renderProcessesOnListView()
+        {
+            listView1.Items.Clear();
+            BgFunctions bgfunctions = new BgFunctions();
+            // Create an array to store the processes
+            processList = Process.GetProcesses();
+
+            // Create an Imagelist that will store the icons of every process
+            ImageList Imagelist = new ImageList();
+            TimeSpan runtime = new TimeSpan(11, 12, 52); // H M S
+
+
+            // Loop through the array of processes to show information of every process in your console
+            foreach (Process process in processList)
+            {
+                if (bgfunctions.inProcesses(process.ProcessName))
+                {
+                    // Define the status from a boolean to a simple string
+                    string status = (process.Responding == true ? "Responding" : "Not responding");
+                    if (process.ProcessName == "chrome" && process.MainWindowTitle.Length < 5) { continue; } // remove duplicates of chrome
+                    if (process.ProcessName == "Teams" && process.MainWindowTitle.Length < 5) { continue; } // remove duplicates of Teams
+                    try
+                    {
+                        runtime = DateTime.Now - process.StartTime;
+                    }
+                    // catching error if any, mainly access denied
+                    catch (Win32Exception ex)
+                    {
+                        // Ignore processes that give "access denied" error.
+                        if (ex.NativeErrorCode == 5)
+                            continue;
+                        throw;
+                    }
+                    catch (InvalidOperationException )
+                    {
+                        continue;
+                    }
+                    // Create an array of string that will store the information to display in our 
+                    string[] row = {
+                        // 1 Process name
+                        process.ProcessName,
+                        // 2 Process Time
+                        $"{runtime:hh\\:mm\\:ss}",
+                        // 3 Process status
+                        status,
+                        // 4 Memory usage
+                        BytesToReadableValue(process.PrivateMemorySize64),
+                        // 5 Process ID
+                        process.Id.ToString(),
+                    };
+
+                    //
+                    // As not every process has an icon then, prevent the app from crash
+                    try
+                    {
+                        Imagelist.Images.Add(
+                            // Add an unique Key as identifier for the icon (same as the ID of the process)
+                            process.Id.ToString(),
+                            // Add Icon to the List 
+                            Icon.ExtractAssociatedIcon(process.MainModule.FileName).ToBitmap()
+                        );
+                    }
+                    catch { }
+
+                    // Create a new Item to add into the list view that expects the row of information as first argument
+                    ListViewItem item = new ListViewItem(row)
+                    {
+                        // Set the ImageIndex of the item as the same defined in the previous try-catch
+                        ImageIndex = Imagelist.Images.IndexOfKey(process.Id.ToString())
+                    };
+
+                    // Add the Item
+                    listView1.Items.Add(item);
+                }
+
+            }
+
+            // Set the imagelist of your list view the previous created list :)
+            listView1.LargeImageList = Imagelist;
+            listView1.SmallImageList = Imagelist;
+        }
+
+        // Method that converts bytes to its human readable value
+        public string BytesToReadableValue(long number)
+        {
+            List<string> suffixes = new List<string> { " B", " KB", " MB", " GB", " TB", " PB" };
+
+            for (int i = 0; i < suffixes.Count; i++)
+            {
+                long temp = number / (int)Math.Pow(1024, i + 1);
+
+                if (temp == 0)
+                {
+                    return (number / (int)Math.Pow(1024, i)) + suffixes[i];
+                }
+            }
+
+            return number.ToString();
+        }
+
+        // Click events
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Exited");
+            Application.Exit();
+            quitToolStripMenuItem.Visible = false;
+        }
+        private void chromeToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            foreach (Process process in processList)
+            {
+                if (process.ProcessName == "chrome") { process.Kill(); Console.WriteLine("Killed chrome"); chromeToolStripMenuItem.Visible = false; }
+            }
+        }
+        private void csgoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Process process in processList)
+            {
+                if (process.ProcessName == "csgo")
+                {
+                    process.Kill(); Console.WriteLine("Killed csgo"); csgoToolStripMenuItem.Visible = false;
+                }
+            }
+        }
+        private void CitiesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Process process in processList)
+            {
+                if (process.ProcessName == "Cities") { process.Kill(); Console.WriteLine("Killed Cities"); CitiesToolStripMenuItem.Visible = false; }
+            }
+        }
+        private void BannerlordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Process process in processList)
+            {
+                if (process.ProcessName == "Bannerlord") { process.Kill(); Console.WriteLine("Killed Bannerlord"); BannerlordToolStripMenuItem.Visible = false; }
+            }
+        }
+        private void steamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Process process in processList)
+            {
+                if (process.ProcessName == "steam") { process.Kill(); Console.WriteLine("Killed steam"); steamToolStripMenuItem.Visible = false; }
+            }
+        }
+        private void teamsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Process process in processList)
+            {
+                if (process.ProcessName == "Teams") { process.Kill(); Console.WriteLine("Killed Teams"); teamsToolStripMenuItem.Visible = false; }
+            }
+        }
+
+        // making menustripitem visible if the process exists
+        public int processesInNotifyIcon(Process[] processList)
+        {
+            foreach (Process process in processList)
+            {
+                switch (process.ProcessName)
+                {
+                    case "chrome":
+                        chromeToolStripMenuItem.Visible = true;
+                        break;
+                    case "csgo":
+                        csgoToolStripMenuItem.Visible = true;
+                        break;
+                    case "Cities":
+                        CitiesToolStripMenuItem.Visible = true;
+                        break;
+                    case "Bannerlord":
+                        BannerlordToolStripMenuItem.Visible = true;
+                        break;
+                    case "steam":
+                        steamToolStripMenuItem.Visible = true;
+                        break;
+                    case "Teams":
+                        teamsToolStripMenuItem.Visible = true;
+                        break;
+                }
+            }
+            return 0;
+        }
+
+        public int refreshEvent()
+        {
+            renderProcessesOnListView();
+            processesInNotifyIcon(processList);
+            return 0;
+        }
+    }
+}
